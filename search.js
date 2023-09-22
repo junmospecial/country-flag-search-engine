e3bec7109ff43995ace88f59c1e9236d64d54a18
JunaidMohammed <jmoham2@uwo.ca>
2023-09-22 10:14:10 -0400

    CommentedCode

--- search.js
+++ search.js
@@ -1,140 +1,114 @@
+// This event listener waits for the DOM content to be fully loaded before executing the JavaScript code
 document.addEventListener('DOMContentLoaded', function () {
+  // Get references to the input elements by their IDs
   const NameInput = document.getElementById('gsearchByName');
   const CurrencyInput = document.getElementById('gsearchByCurrency');
+
+  // Function to search for countries by name
   function searchCountries() {
-    const searchText = NameInput.value.trim().toLowerCase(); // Trim whitespace
-  
+    // Get the trimmed and lowercase search text from the name input field
+    const searchText = NameInput.value.trim().toLowerCase();
+
+    // Check if the input is empty and show an alert if it is
     if (searchText === '') {
       alert('Input is empty. Please enter a search term.');
       return; // Exit the function if input is empty
     }
-  
+
+    // Select all elements with class 'country-box'
     const countries = document.querySelectorAll('.country-box');
     let matches = [];
-  
+
+    // Loop through each country box
     countries.forEach(country => {
+      // Get the lowercase text content of the h2 element within the country box
       const cName = country.querySelector('h2').textContent.toLowerCase();
-  
+
+      // Check if the country name starts with the search text
       if (cName.startsWith(searchText)) {
+        // Get the currency information from the country box
         const currency = country.querySelector('ul li:nth-child(1)').textContent;
         matches.push({ cName, currency });
       }
     });
-  
+
+    // Display the matching countries and their currencies in an alert
     if (matches.length > 0) {
       let alertContent = '';
-  
+
       for (let i = 0; i < Math.min(matches.length, 5); i++) {
         alertContent += `${matches[i].cName} - ${matches[i].currency}\n`;
       }
-  
+
       alert(alertContent);
     } else {
       alert('No matches found.');
     }
   }
-  
 
+  // Function to search for countries by currency
   function searchCurrency() {
-    const CurrencyName = CurrencyInput.value.toUpperCase();
-  
-    if (!CurrencyName.trim()) {
-      alert('Please enter a currency you would like to find.');
-      return;
-    }
-  
-      function searchCurrency() {
+    // Get the trimmed currency name from the currency input field
     const CurrencyName = CurrencyInput.value.trim();
-  
+
+    // Check if the input is empty and show an alert if it is
     if (!CurrencyName.trim()) {
       alert('Please enter a currency you would like to find.');
       return;
     }
-  
+
+    // Check if the input matches the expected format of exactly 3 capital letters
     if (!/^[A-Z]{3}$/.test(CurrencyName)) {
       alert('Please enter exactly 3 capital letters.');
       return;
-  }
-  
+    }
+
+    // Use the entered currency name as the search text
     const searchText = CurrencyName;
     const countries = document.querySelectorAll('.country-box');
-  
     let matches = [];
-  
+
+    // Loop through each country box
     countries.forEach(country => {
       const currencyItems = country.querySelectorAll('ul li');
       let currency = null;
-  
+
+      // Find the currency information within the list items
       currencyItems.forEach(item => {
         if (item.textContent.startsWith('Currency:')) {
           currency = item.textContent;
         }
       });
-  
-      if (currency && currency.includes(searchText)) {
-        const cName = country.querySelector('h2').textContent;
-        matches.push({ cName, currency });
-      }
-    });
-  
-    if (matches.length > 0) {
-      let alertContent = '';
-  
-      for (let i = 0; i < Math.min(matches.length, 5); i++) {
-        alertContent += `${matches[i].cName} - ${matches[i].currency}\n`;
-      }
-      alert(alertContent);
-    } else {
-      alert('No matches found.');
-    }
-    
-  }
-  
-  
-    const searchText = CurrencyName.toLowerCase();
-    const countries = document.querySelectorAll('.country-box');
-  
-    let matches = [];
-  
-    countries.forEach(country => {
-      const currencyItems = country.querySelectorAll('ul li');
-      let currency = null;
-  
-      currencyItems.forEach(item => {
-        if (item.textContent.startsWith('Currency:')) {
-          currency = item.textContent.toLowerCase();
-        }
-      });
-  
-      if (currency && currency.includes(searchText)) {
-        const cName = country.querySelector('h2').textContent;
-        matches.push({ cName, currency });
-      }
-    });
-  
-    if (matches.length > 0) {
-      let alertContent = '';
-  
-      for (let i = 0; i < Math.min(matches.length, 5); i++) {
-        alertContent += `${matches[i].cName} - ${matches[i].currency}\n`;
-      }
-  
-      alert(alertContent);
-    } else {
-      alert('No matches found.');
-    }
-    
-  }
-  
 
-  // Add event listener for search button click
+      // Check if the currency information includes the search text
+      if (currency && currency.includes(searchText)) {
+        const cName = country.querySelector('h2').textContent;
+        matches.push({ cName, currency });
+      }
+    });
+
+    // Display the matching countries and their currencies in an alert
+    if (matches.length > 0) {
+      let alertContent = '';
+
+      for (let i = 0; i < Math.min(matches.length, 5); i++) {
+        alertContent += `${matches[i].cName} - ${matches[i].currency}\n`;
+      }
+
+      alert(alertContent);
+    } else {
+      alert('No matches found.');
+    }
+  }
+
+  // Add event listener for search button click for name search
   const searchButton = document.getElementById('searchByName');
   searchButton.addEventListener('click', function (event) {
     event.preventDefault(); // Prevents form submission
     searchCountries();
   });
 
-  // Add event listener for enter key press
+  // Add event listener for Enter key press in name input field
   NameInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
       event.preventDefault(); // Prevents form submission
@@ -142,18 +116,18 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  // Add event listener for search button click
+  // Add event listener for search button click for currency search
   const searchButton2 = document.getElementById('searchByCurrency');
   searchButton2.addEventListener('click', function (event) {
     event.preventDefault(); // Prevents form submission
     searchCurrency();
   });
 
+  // Add event listener for Enter key press in currency input field
   CurrencyInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
       event.preventDefault(); // Prevents form submission
       searchCurrency();
     }
   });
-
 });
