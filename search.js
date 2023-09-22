34180c415299149662b8e64a223e5d47e7643bc5
JunaidMohammed <jmoham2@uwo.ca>
2023-09-22 09:40:29 -0400

    searchForCountryFIxed

--- search.js
+++ search.js
@@ -1,121 +1,69 @@
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const searchInput = document.getElementById('gsearchByName');
-        const searchInput1 = document.getElementById('gsearchByCurrency');
-    
-        function searchCountries() {
-            const searchText = searchInput.value.toLowerCase();
-            const countries = document.querySelectorAll('.country-box');
-    
-            let matches = [];
-    
-            countries.forEach(country => {
-                const countryName = country.querySelector('h2').textContent.toLowerCase();
-    
-                if (countryName.startsWith(searchText)) {
-                    const currency = country.querySelector('ul li:nth-child(1)').textContent;
-                    matches.push({ countryName, currency });
-                }
-            });
-    
-            if (matches.length > 0) {
-                let alertContent = '';
-    
-                for (let i = 0; i < Math.min(matches.length, 5); i++) {
-                    alertContent += `${matches[i].countryName} - ${matches[i].currency}\n`;
-
-                }
-    
-                alert(alertContent);
-            }
-        } 
-        // Add event listener for search button click
-        const searchButton = document.getElementById('searchByName');
-        searchButton.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevents form submission     
-        });
-        // Add event listener for enter key press
-        searchInput.addEventListener('keydown', function(event) {
-            if (event.key === 'Enter') {
-                event.preventDefault(); // Prevents form submission
-                searchCountries();
-            }
-        });  
-        searchButton.addEventListener('click', function(event) {
-          event.preventDefault(); // Prevents form submission
-          if (searchInput.value.trim() === '') {
-              alert('Input is empty. Please enter a search term.');
-          } else {
-              searchCountries();
-          }
-      });
-    });
-
-
-
-
-
-
-    
-    document.addEventListener('DOMContentLoaded', function() {
-      const searchInput = document.getElementById('gsearchByName');
-      const searchInput1 = document.getElementById('gsearchByCurrency');
+document.addEventListener('DOMContentLoaded', function () {
+  const NameInput = document.getElementById('gsearchByName');
+  const CurrencyInput = document.getElementById('gsearchByCurrency');
+  function searchCountries() {
+    const searchText = NameInput.value.trim().toLowerCase(); // Trim whitespace
   
-      function searchCurrencies() {
-          const searchText = searchInput1.value.toLowerCase();
-          const currencies = document.querySelectorAll('.cCode');
+    if (searchText === '') {
+      alert('Input is empty. Please enter a search term.');
+      return; // Exit the function if input is empty
+    }
   
-          let matches = [];
+    const countries = document.querySelectorAll('.country-box');
+    let matches = [];
   
-          currencies.forEach(country => {
-              const countryName = country.querySelector('h2').textContent.toLowerCase();
-              const countryCurrency = country.querySelector('li').textContent.toLowerCase();
+    countries.forEach(country => {
+      const cName = country.querySelector('h2').textContent.toLowerCase();
   
-              if (countryName.startsWith(searchText)) {
-                  const currency = country.querySelector('ul li:nth-child(1)').textContent;
-                  matches.push({ countryName, currency });
-              }
-          });
-  
-          if (matches.length > 0) {
-              let alertContent = '';
-  
-              for (let i = 0; i < Math.min(matches.length, 5); i++) {
-                  alertContent += `${matches[i].countryName} - ${matches[i].currency}\n`;
-
-              }
-  
-              alert(alertContent);
-          }
+      if (cName.startsWith(searchText)) {
+        const currency = country.querySelector('ul li:nth-child(1)').textContent;
+        matches.push({ cName, currency });
       }
-
-
-
-      // Add event listener for search button click
-      const searchButton2 = document.getElementById('searchByCurrency');
-      searchButton2.addEventListener('click', function(event) {
-          event.preventDefault(); // Prevents form submission
-          
-      });
-  
-      // Add event listener for enter key press
-      searchInput1.addEventListener('keydown', function(event) {
-          if (event.key === 'Enter') {
-              event.preventDefault(); // Prevents form submission
-              searchCurrencies();
-          }
-      });
-  
-      searchButton2.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevents form submission
-        if (searchInput1.value.trim() === '') {
-            alert('Input is empty. Please enter a search term.');
-        } else {
-            searchCurrencies();
-        }
     });
+  
+    if (matches.length > 0) {
+      let alertContent = '';
+  
+      for (let i = 0; i < Math.min(matches.length, 5); i++) {
+        alertContent += `${matches[i].cName} - ${matches[i].currency}\n`;
+      }
+  
+      alert(alertContent);
+    } else {
+      alert('No matches found.');
+    }
+  }
+  
+
+
+
+  // Add event listener for search button click
+  const searchButton = document.getElementById('searchByName');
+  searchButton.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevents form submission
+    searchCountries();
   });
-    
 
+  // Add event listener for enter key press
+  NameInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevents form submission
+      searchCountries();
+    }
+  });
 
+  // Add event listener for search button click
+  const searchButton2 = document.getElementById('searchByCurrency');
+  searchButton2.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevents form submission
+    searchCurrency();
+  });
 
+  CurrencyInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevents form submission
+      searchCurrency();
+    }
+  });
+
+});
